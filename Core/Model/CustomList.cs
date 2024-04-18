e25f6e39a509b615ac4f4d836d080c0749e865f6
Aytac Israfilova <bd7k6z9ku@code.edu.az>
2024-04-18 11:13:43 +0400

    Add project files.

---
+++ Core/Model/CustomList.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Model
+{
+    public class CustomList<T>
+    {
+        private T[] items;
+        private int _count;
+        private int _capacity;
+
+        public T this[int index] => items[index];
+        public int Count => _count;
+        public int Capacity => _capacity;
+
+
+        public void Add(T item)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                if (_count == _capacity)
+                {
+                    _capacity *= 2;
+                }
+                    
+
+                Array.Resize(ref items, items.Length + 1);
+                items[items.Length] = item;
+            }
+        }
+        
+        public T Find(Predicate<T> method)
+        {
+            foreach (T item in items)
+            {
+                if (method(item))
+                    return item;
+            }
+            return default;
+        }
+
+        public List<T> FindAll(Predicate<T> match)
+        {
+            List<T> foundItems = new List<T>();
+            foreach (T item in items)
+            {
+                if (match(item))
+                    foundItems.Add(item);
+            }
+            return foundItems;
+        }
+
+        public void RemoveAll(Predicate<T> match)
+        {
+            for (int i = 0; i < _count; i++)
+            {
+                if (match(items[i]))
+                {
+
+                    i--;
+                }
+            }
+        }
+
+        
+
+        public void ForEach(Action<T> action)
+        {
+            foreach (T item in items)
+            {
+                action(item);
+            }
+        }
+
+    }
+    
+}
---
+++ customlıst/Program.cs
@@ -0,0 +1,44 @@
+﻿using Core.Model;
+
+namespace customlıst
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            CustomList<int> customList = new CustomList<int>();
+
+            
+            customList.Add(10);
+            customList.Add(20);
+            customList.Add(30);
+            customList.Add(40); 
+
+            
+            Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+           
+            int elementToFind = 20;
+            int foundElement = customList.Find(x => x == elementToFind);
+            Console.WriteLine($"Found element {elementToFind}: {foundElement}");
+
+           
+            var elementsGreaterThan20 = customList.FindAll(x => x > 20);
+            Console.WriteLine("Elements greater than 20:");
+            foreach (var item in elementsGreaterThan20)
+            {
+                Console.WriteLine(item);
+            }
+
+            
+            Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+
+            
+            customList.RemoveAll(x => x > 20);
+
+            
+            Console.WriteLine($"Count: {customList.Count}, Capacity: {customList.Capacity}");
+        }
+    }
+}
+ 
